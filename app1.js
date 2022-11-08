fb8d79d4d5f48044c52599f1ff5d966e2741aa48
obikakelvin <obikakelvin.2255@gmail.com>
2022-11-08 13:17:38 -0800

    initial commit

--- app1.js
+++ app1.js
@@ -37,14 +37,14 @@ window.addEventListener("load", cameraStart, false);
 
 
 // Install ServiceWorker
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
-    console.log('CLIENT: service worker registration complete.');
-  }, function() {
-    console.log('CLIENT: service worker registration failure.');
-  });
-} else {
-  console.log('CLIENT: service worker is not supported.');
-}
+// if ('serviceWorker' in navigator) {
+//   console.log('CLIENT: service worker registration in progress.');
+//   navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+//     console.log('CLIENT: service worker registration complete.');
+//   }, function() {
+//     console.log('CLIENT: service worker registration failure.');
+//   });
+// } else {
+//   console.log('CLIENT: service worker is not supported.');
+// }
 
